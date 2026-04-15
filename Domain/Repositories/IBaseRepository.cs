da37d26ed2ce2779f736de1a4419694d4e4f230f
Yosandi <elyosandireyes@gmail.com>
2026-04-14 22:03:47 -0400

    Domain terminado

--- Domain/Repositories/IBaseRepository.cs
+++ Domain/Repositories/IBaseRepository.cs
@@ -1,7 +1,16 @@
-﻿namespace Domain.Repositories
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Domain.Repositories
 {
-    public interface IBaseRepository
+    public interface IBaseRepository <TEntity, TId> where TEntity : class
     {
+        
+        TEntity Get(TId id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        void Add(TEntity entity);
+        void Delete(TEntity entity);
 
     }
 }
---
+++ Persistece/Base/BaseRepository.cs
@@ -0,0 +1,40 @@
+﻿
+
+using Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistece.Base
+{
+    public abstract class BaseRepository<TEntity, TId> : IBaseRepository<TEntity, TId> where TEntity : class
+    {
+        protected readonly DbContext Context;
+        protected BaseRepository(DbContext context)
+        {
+            Context = context;
+        }
+        public void Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> Find(System.Linq.Expressions.Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(TId id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
