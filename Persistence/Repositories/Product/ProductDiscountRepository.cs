9816ce2fe3dfd892348a4d170b4abab9616e1085
Yosandi Miguel Reyes <80928311+YosandiMiguelReyes@users.noreply.github.com>
2026-07-20 21:13:11 +0000

    now only using a single mapper for product and productwithdiscount

--- Persistence/Repositories/Product/ProductDiscountRepository.cs
+++ Persistence/Repositories/Product/ProductDiscountRepository.cs
@@ -22,55 +22,63 @@ namespace Persistence.Repositories.Product
         public async Task<IEnumerable<AdminProductWithDiscountDTO>> AdminGetAllProductsWithDiscountsAsync()
         {
             return await _dbSet
-                .Select(ProductWithDiscountMapper.AsAdminProductWithDiscount).ToListAsync();
+                .Where(pd => pd.IsActive)
+                .Select(Pd => Pd.Product)
+                .Select(ProductstMapper.AsAdminProductWithDiscount).ToListAsync();
         }
 
         public async Task<IEnumerable<AdminProductWithDiscountDTO>> AdminGetAllProductswithDiscountbyEndDateAsync(DateTime endDate)
         {
             return await _dbSet
-                .Where(pd => pd.EndDate <= endDate.Date)
-                .Select(ProductWithDiscountMapper.AsAdminProductWithDiscount).ToListAsync();
+                .Where(pd => pd.IsActive && pd.EndDate <= endDate.Date)
+                .Select(pd => pd.Product)
+                .Select(ProductstMapper.AsAdminProductWithDiscount).ToListAsync();
         }
 
         public async Task<IEnumerable<AdminProductWithDiscountDTO>> AdminGetAllProductswithDiscountbyStartDateAsync(DateTime startDate)
         {
             return await _dbSet
-                .Where(pd => pd.StartDate >= startDate.Date)
-                .Select(ProductWithDiscountMapper.AsAdminProductWithDiscount).ToListAsync();
+                .Where(pd => pd.IsActive && pd.StartDate >= startDate.Date)
+                .Select(pd => pd.Product)
+                .Select(ProductstMapper.AsAdminProductWithDiscount).ToListAsync();
         }
 
         public async Task<IEnumerable<AdminProductWithDiscountDTO>> AdminGetAllProductsWithDiscountsByDateRangeAsync(DateTime startDate, DateTime endDate)
         {
             return await _dbSet
-                .Where(pd => pd.StartDate >= startDate.Date 
+                .Where(pd => pd.IsActive && pd.StartDate >= startDate.Date 
                         && pd.EndDate <= endDate.Date)
-                .Select(ProductWithDiscountMapper.AsAdminProductWithDiscount).ToListAsync();
+                .Select(pd => pd.Product)
+                .Select(ProductstMapper.AsAdminProductWithDiscount).ToListAsync();
         }
 
         public async Task<IEnumerable<AdminProductWithDiscountDTO>> AdminGetAllProductsWithDiscountsByPercentageAsync(decimal percentage)
         {
             return await _dbSet
-                .Where(pd => pd.Percentage.HasValue 
+                .Where(pd => pd.IsActive && pd.Percentage.HasValue 
                         && pd.Percentage == percentage)
-                .Select(ProductWithDiscountMapper.AsAdminProductWithDiscount).ToListAsync();
+                .Select(pd => pd.Product)
+                .Select(ProductstMapper.AsAdminProductWithDiscount).ToListAsync();
         }
 
         public async Task<IEnumerable<AdminProductWithDiscountDTO>> AdminGetAllProductWithDiscountByNameAsync(string name)
         {
             return await _dbSet
-                .Where(pd => pd.Product.Name.Contains(name))
-                .Select(ProductWithDiscountMapper.AsAdminProductWithDiscount).ToListAsync();
+                .Where(pd => pd.IsActive && pd.Product.Name.Contains(name))
+                .Select(pd => pd.Product)
+                .Select(ProductstMapper.AsAdminProductWithDiscount).ToListAsync();
         }
 
         public async Task<IEnumerable<ProductWithDiscountDTO>> GetActiveProductsWithDiscountsAsync()
         {
             var today = DateTime.UtcNow.Date;
             return await _dbSet
-                .Where(pd => pd.IsActive == true
+                .Where(pd => pd.IsActive
                 && pd.StartDate <= today
                 && pd.EndDate >= today
                 && pd.Product.IsActive == true)
-                .Select(ProductWithDiscountMapper.AsProductWithDiscount).ToListAsync();
+                .Select(pd => pd.Product)
+                .Select(ProductstMapper.AsProductWithDiscount).ToListAsync();
         }
 
         public async Task<IEnumerable<ProductWithDiscountDTO>> GetProductswithDiscountbyEndDateAsync(DateTime endDate)
@@ -80,7 +88,8 @@ namespace Persistence.Repositories.Product
                 && pd.IsActive == true
                 && pd.EndDate >= DateTime.Today
                 && pd.Product.IsActive == true)
-                .Select(ProductWithDiscountMapper.AsProductWithDiscount).ToListAsync();
+                .Select(pd => pd.Product)
+                .Select(ProductstMapper.AsProductWithDiscount).ToListAsync();
         }
 
         public async Task<IEnumerable<ProductWithDiscountDTO>> GetProductswithDiscountbyStartDateAsync(DateTime startDate)
@@ -89,7 +98,8 @@ namespace Persistence.Repositories.Product
                 .Where(pd => pd.StartDate >= startDate 
                 && pd.IsActive == true
                 && pd.Product.IsActive == true)
-                .Select(ProductWithDiscountMapper.AsProductWithDiscount).ToListAsync();
+                .Select(pd => pd.Product)
+                .Select(ProductstMapper.AsProductWithDiscount).ToListAsync();
         }
 
         public async Task<IEnumerable<ProductWithDiscountDTO>> GetProductsWithDiscountsByDateRangeAsync(DateTime startDate, DateTime endDate)
@@ -99,7 +109,8 @@ namespace Persistence.Repositories.Product
                 && pd.EndDate <= endDate 
                 && pd.IsActive == true
                 && pd.Product.IsActive == true)
-                .Select(ProductWithDiscountMapper.AsProductWithDiscount).ToListAsync();
+                .Select(pd => pd.Product)
+                .Select(ProductstMapper.AsProductWithDiscount).ToListAsync();
         }
 
         public async Task<IEnumerable<ProductWithDiscountDTO>> GetProductsWithDiscountsByPercentageAsync(decimal percentage)
@@ -109,7 +120,8 @@ namespace Persistence.Repositories.Product
                 && pd.Percentage == percentage 
                 && pd.IsActive == true
                 && pd.Product.IsActive == true)
-                .Select(ProductWithDiscountMapper.AsProductWithDiscount).ToListAsync();
+                .Select(pd => pd.Product)
+                .Select(ProductstMapper.AsProductWithDiscount).ToListAsync();
         }
 
         public async Task<IEnumerable<ProductWithDiscountDTO>> GetProductWithDiscountByNameAsync(string name)
@@ -118,7 +130,8 @@ namespace Persistence.Repositories.Product
                 .Where(pd => pd.Product.Name.Contains(name) 
                 && pd.IsActive == true
                 && pd.Product.IsActive == true)
-                .Select(ProductWithDiscountMapper.AsProductWithDiscount).ToListAsync();
+                .Select(pd => pd.Product)
+                .Select(ProductstMapper.AsProductWithDiscount).ToListAsync();
         }
     }
 }
