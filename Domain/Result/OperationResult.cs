3ab0dad39d080a3d43967e12615a197dfa97d5ab
Yosandi Miguel Reyes <80928311+YosandiMiguelReyes@users.noreply.github.com>
2026-08-06 14:54:13 +0000

    Operation Result deleted from Domain to later implement in Application

--- Domain/Result/OperationResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.Result
-{
-    public class OperationResult<T>
-    {
-        public T Data { get; set; }
-        public bool IsSuccess { get; set; }
-        public string Message { get; set; }
-    }
-}
