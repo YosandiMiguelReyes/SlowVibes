62a26422a59557a1ce52fb9d971e553c3ab43399
Yosandi Miguel Reyes <80928311+YosandiMiguelReyes@users.noreply.github.com>
2026-09-06 21:16:22 +0000

    order repo completed

--- Persistence/Repositories/Order/OrderRepository.cs
+++ Persistence/Repositories/Order/OrderRepository.cs
@@ -6,6 +6,7 @@ using Domain.Entities.Order.Enums;
 using Persistence.BaseRepository;
 using Persistence.Context;
 using Persistence.Interfaces.Order;
+using Microsoft.EntityFrameworkCore;
 
 namespace Persistence.Repositories.Order
 {
@@ -16,64 +17,97 @@ namespace Persistence.Repositories.Order
             
         }
 
-        public Task<IEnumerable<Orders>> GetOrderByDateRange(DateTimeOffset startDate, DateTimeOffset endDate)
+        public async Task<IEnumerable<Orders>> GetOrderByDateRange(DateTimeOffset startDate, DateTimeOffset endDate)
         {
-            throw new NotImplementedException();
+            return await _dbSet
+                .AsNoTracking()
+                .Where(o => o.OrderDate >= startDate && o.OrderDate <= endDate)
+                .ToListAsync();
         }
 
-        public Task<IEnumerable<Orders>> GetOrdersByCustomerPhoneAsync(string customerPhone)
+        public async Task<IEnumerable<Orders>> GetOrdersByCustomerPhoneAsync(string customerPhone)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<Orders>> GetOrdersByDeliveryTypeAsync(DeliveryTypes deliveryType)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<Orders>> GetOrdersBySourceAsync(OrderSources source)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<Orders>> GetOrdersByStatusAsync(OrderStatuses status)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<Orders>> GetOrdersByTotalAmountRangeAsync(decimal minAmount, decimal maxAmount)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<Orders>> GetOrdersByTotalProfitRangeAsync(decimal minAmount, decimal maxAmount)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<Orders>> GetOrdersByUserNameAsync(string userName)
-        {
-            var query = 
-                from order in _dbSet
-                join user in _context.Set<Users>()
+            return await(
+                from order in _dbSet.AsNoTracking()
+                join user in _context.Set<Domain.Entities.User.Users>().AsNoTracking()
                 on order.UserId equals user.Id
-                where(userName == user.userName)
-                select order;
+                where user.Phone == customerPhone
+                select order).ToListAsync();
         }
 
-        public Task<IEnumerable<Orders>> GetOrdersOrderByDateAsync(bool ascending)
+        public async Task<IEnumerable<Orders>> GetOrdersByDeliveryTypeAsync(DeliveryTypes deliveryType)
         {
-            throw new NotImplementedException();
+            return await _dbSet
+                .AsNoTracking()
+                .Where(o => o.DeliveryType == deliveryType)
+                .ToListAsync();
         }
 
-        public Task<IEnumerable<Orders>> GetOrdersOrderByTotalAmountAsync(bool ascending)
+        public async Task<IEnumerable<Orders>> GetOrdersBySourceAsync(OrderSources source)
         {
-            throw new NotImplementedException();
+            return await _dbSet
+                .AsNoTracking()
+                .Where(o => o.OrderSource == source)
+                .ToListAsync();
+        }
+        
+
+        public async Task<IEnumerable<Orders>> GetOrdersByStatusAsync(OrderStatuses status)
+        {
+            return await _dbSet
+                .AsNoTracking()
+                .Where(o => o.OrderStatus == status)
+                .ToListAsync();
         }
 
-        public Task<IEnumerable<Orders>> GetOrdersOrderByTotalProfitAsync(bool ascending)
+        public async Task<IEnumerable<Orders>> GetOrdersByTotalAmountRangeAsync(decimal minAmount, decimal maxAmount)
         {
-            throw new NotImplementedException();
+            return await _dbSet
+                .AsNoTracking()
+                .Where(o => o.TotalAmount >= minAmount && o.TotalAmount <= maxAmount)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Orders>> GetOrdersByTotalProfitRangeAsync(decimal minAmount, decimal maxAmount)
+        {
+            return await _dbSet
+                .AsNoTracking()
+                .Where(o => o.TotalProfit >= minAmount && o.TotalProfit <= maxAmount)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Orders>> GetOrdersByUserNameAsync(string userName)
+        {
+            return await(
+                from order in _dbSet.AsNoTracking()
+                join user in _context.Set<Domain.Entities.User.Users>().AsNoTracking()
+                on order.UserId equals user.Id
+                where user.UserName == userName
+                select order).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Orders>> GetOrdersOrderByDateAsync(bool ascending)
+        {
+            var query = _dbSet.AsNoTracking();
+
+            query = ascending ? query.OrderBy(o => o.OrderDate) : query.OrderByDescending(o => o.OrderDate);
+            return await query.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Orders>> GetOrdersOrderByTotalAmountAsync(bool ascending)
+        {
+            var query = _dbSet.AsNoTracking();
+
+            query = ascending ? query.OrderBy(o => o.TotalAmount) : query.OrderByDescending(o => o.TotalAmount);
+            return await query.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Orders>> GetOrdersOrderByTotalProfitAsync(bool ascending)
+        {
+            var query = _dbSet.AsNoTracking();
+
+            query = ascending ? query.OrderBy(o => o.TotalProfit) : query.OrderByDescending(o => o.TotalProfit);
+            return await query.ToListAsync();
         }
     }
 }
